e2f78e89328f4825f289a1e60b3227b2b87ff35a
Ajitesh-Tamrakar <ajiteshtamrakar15@gmail.com>
2026-05-19 15:30:13 +0530

    mirror bug fixes

--- process/management/commands/run_worker.py
+++ process/management/commands/run_worker.py
@@ -25,13 +25,11 @@ class Command(BaseCommand):
                         Bookmark.objects
                         .select_for_update()
                         .filter(id=pending.id)
-                    )             
+                    )
 
-                    process.processing_status = 'processing'
-                    
-                    process.save()
-        
-                run_pipeline(process)
+                    process.update(processing_status='processing')             
+
+                run_pipeline(pending)
 
 
                 time.sleep(5)
@@ -39,28 +37,3 @@ class Command(BaseCommand):
             self.stdout.write('Forced stop: keyboard interrupt')
         except Exception as e:
             self.stdout.write(f'error {str(e)}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # for process in stuck_processes:
-        #     bad_process = Bookmark(id = process.id)
-        #     if bad_process.processing_status == 'processing':
-        #         Bookmark.objects.filter(id = process.id).update(
-        #             processing_status = 'pending'
-        #         )
