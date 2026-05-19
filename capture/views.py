b41f4da43d0c4f4614dc20380126aec086c62938
Ajitesh-Tamrakar <ajiteshtamrakar15@gmail.com>
2026-05-19 15:29:21 +0530

    added capture logic

--- capture/views.py
+++ capture/views.py
@@ -1,3 +1,62 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from core.models import Bookmark
+import sqlite3
+import os
+from pathlib import Path
 
 # Create your views here.
+@csrf_exempt
+def capture(request):
+    if request.method == 'POST':
+        body = request.body
+        data = json.loads(body)
+
+        try:
+            required_fields = ['url', 'title', 'platform', 'content_type', 'capture_method', 'author', 'author_link']
+            for field in required_fields:
+                if field not in data:
+                    return JsonResponse({'error': f'Missing required field: {field}'}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+        
+        Bookmark.objects.create(
+            url = data['url'],
+            title = data['title'],
+            platform = data['platform'],
+            content_type = data['content_type'],
+            capture_method = data['capture_method'],
+            author = data['author'],
+            # author_link = data['author_link']
+        )
+
+        try:
+            project_root = Path(__file__).parent.parent
+            os.makedirs(project_root / 'temp', exist_ok=True)
+            conn = sqlite3.connect(f'{project_root}/temp/db.sqlite3')
+        except FileNotFoundError:
+            print("Database file not found.")
+            return JsonResponse({'error': 'Database file not found'}, status=500)
+        except sqlite3.Error as e:
+            print(f"Error connecting to database: {e}")
+            return JsonResponse({'error': 'Database connection error'}, status=500)
+        
+        conn.execute('''CREATE TABLE IF NOT EXISTS capture_log
+                     (url TEXT,
+                     title TEXT,
+                     platform TEXT,
+                     content_type TEXT,
+                     capture_method TEXT,
+                     author TEXT,
+                     author_link TEXT,
+                     note TEXT, 
+                     captured_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
+        conn.execute('''INSERT INTO capture_log (url, title, platform, content_type, capture_method, author, author_link)
+                     VALUES (?, ?, ?, ?, ?, ?, ?)''', (data['url'], data['title'], data['platform'], data['content_type'], data['capture_method'], data['author'], data['author_link']))
+        conn.commit()
+
+        return JsonResponse({'message': 'Data captured successfully'}, status=200)
+    
+    return JsonResponse({'error': "Post method only "}, status=400)
